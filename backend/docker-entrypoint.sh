a59f670bfdb043e0d76f13d71915b181825cdf17
RomSTil <dintermaksim5@gmail.com>
2026-07-23 17:35:34 +0300

    Fix backend entrypoint for worker commands

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -5,7 +5,7 @@ mkdir -p /data/vault
 chown -R taskman:taskman /data /app
 
 if [ "$(id -u)" = "0" ]; then
-  exec su taskman -s /bin/sh -c "$1"
+  exec su taskman -s /bin/sh -c "$*"
 fi
 
 exec "$@"
